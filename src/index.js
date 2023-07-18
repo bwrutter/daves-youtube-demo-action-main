f6cb30d1c5dcb2b3864f117ee3d1e90df11ff8c3
bwrutter <bryanwrutter96@gmail.com>
2023-07-18 18:07:58 -0300

    chore: Retirado os console.log

--- src/index.js
+++ src/index.js
@@ -3,19 +3,15 @@ const b = 20;
 
 function soma(a, b) {
     const resultado = a + b;
-    return `Deu certo a soma viu ${resultado}`;
+    return `Deu certo a soma viu!${resultado}`;
 }
-
-console.log(soma(a, b));
 
 function subtracao(a, b) {
     const resultado = a - b;
-    return `Deu certo a subtração viu ${resultado}`;
+    return `Deu certo a subtração viu!!! ${resultado}`;
 }
-
-console.log(subtracao(a, b));
 
 function multiplicacao(a, b) {
     const resultado = a * b;
-    return `Deu certo a multiplicação viu ${resultado}`;
+    return `Deu certo a multiplicação viu!!! ${resultado + resultado}`;
 }
